55e100fdfbbb081de7c8ae0efd996d3fa290dc03
ncku-yee <asd887722456@gmail.com>
2022-02-20 15:52:16 +0800

    Fix bugs of hw0.py

--- hw0/r10944011_hw0.py
+++ hw0/r10944011_hw0.py
@@ -12,13 +12,15 @@ class Solution:
     def solve(self):
         candidates = [1]                    # Default candidates is 1.
         path = []                           # Path that Capybaras go through. 
-        while(candidates):                  # Herd Capybaras until there is no candidates.
+        while candidates:                   # Herd Capybaras until there is no candidates.
             current = min(candidates)       # Get the candidate in candidates list.
             candidates = []                 # Clear the candidates list.
             path.append(str(current))       # Add candidate to the path.
             for bridge in self.bridges:
-                if(bridge[0] == current and bridge[1] > current):   # Next candidate must larger than current island.
+                if bridge[0] == current and str(bridge[1]) not in path:   # Next candidate must not in the path.
                     candidates.append(bridge[1])
+                if bridge[1] == current and str(bridge[0]) not in path:   # Next candidate must not in the path.
+                    candidates.append(bridge[0])
         print(" ".join(path))
 
 if __name__ == '__main__':
